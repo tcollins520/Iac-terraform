7053ed1260769b673035ea98b9208616bf82fa9a
tcollins520 <tcollins520@gmail.com>
2026-04-24 18:35:16 -0400

    Update AWS instance key name and security group

--- Instance.tf
+++ Instance.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "web" {
   ami                    = data.aws_ami.amiID.id
   instance_type          = "t3.micro"
-  key_name               = "dove-key"
-  vpc_security_group_ids = [aws_security_group.dove-sg.id]
+  key_name               = "bla"
+  vpc_security_group_ids = [aws_security_group.bla.id]
   availability_zone      = "us-east-1a"
 
   tags = {
